97aeb5b6b9a385283c45e51205b2e88daff5640c
AndraGh <36239776+andragh83@users.noreply.github.com>
2018-09-05 15:36:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var item = document.querySelectorAll("li");
 var button_status = document.querySelectorAll(".status");
 var button_delete = document.querySelectorAll(".delete");
 
 
+//checking input
 function inputValueLenght() {
 	return input.value.length;
 }
 
+//creating new shopping with buttons
 function createNewItem() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
@@ -25,33 +26,36 @@ function createNewItem() {
 	li.appendChild(btn2);
 }
 
+//event on clicking Enter button
 function eventOnClick() {
 	if (inputValueLenght()>0){
 	    createNewItem();
-} else {alert("Please add an item!")}
+				} else {
+					alert("Please add an item!")
+					}
 }
 
+//event on Enter key
 function eventOnKeypress(event) {
 	if (inputValueLenght()>0 && event.keyCode === 13){
-	    createNewItem();
-// } else if (inputValueLenght() === 0) {alert("Please add an item!")}
-	}
+	    						createNewItem();
+							}
 }
 
 //delete item function
 function deleteItem() {
 		this.parentNode.remove();
 	}
+
 //Toggle done function
 function doneItem() {
 		this.parentNode.classList.toggle("done");
 	}
 
+//Events
 button.addEventListener("click", eventOnClick);
 input.addEventListener("keypress", eventOnKeypress);
 for (i=0; i<button_delete.length;i++){button_delete[i].addEventListener("click", deleteItem);}
-for (i=0; i<button_status.length;i++){
-	button_status[i].addEventListener("click", doneItem);
-}
+for (i=0; i<button_status.length;i++){button_status[i].addEventListener("click", doneItem);}
 
 
